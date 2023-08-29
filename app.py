3b4f96812da0a8f31eae8ef0b798efab1b150a04
IronMan00702 <140400526+IronMan00702@users.noreply.github.com>
2023-08-29 17:44:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def ask_question():
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
-    serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
